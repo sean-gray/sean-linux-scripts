9cc8d7888205ae740aa4e862b8338315f503a95a
Sean Gray <35218853+sean-gray@users.noreply.github.com>
2021-07-02 19:58:26 -0400

    Add files via upload

---
+++ installGloriousEggroll.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/6.10-GE-1/Proton-6.10-GE-1.tar.gz
+
+mkdir ~/.steam/root/compatibilitytools.d
+tar -xf Proton*.tar.gz -C ~/.steam/root/compatibilitytools.d/
+
+
